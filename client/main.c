1a7f69b265b21225d373b6c3f56880f590971de2
or roob <orroob28@gmail.com>
2021-12-30 13:31:43 +0200

    Add project files.

---
+++ client/main.c
@@ -0,0 +1,214 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h> 
+#pragma comment(lib, "Ws2_32.lib")
+
+
+#define CLIENT_REQUEST 0
+#define CLIENT_VERSUS 1
+#define CLIENT_PLAYER_MOVE 2
+#define CLIENT_DISCONNECT 3
+
+char* PrepareMessage(int messageType, char* arg1, char* arg2, char* arg3)
+{
+	
+	char* buffer;
+	int buffSize;
+	switch (messageType)
+	{
+	case CLIENT_REQUEST:
+		if ((buffSize = snprintf(NULL, 0, "CLIENT_REQUEST:%s\n", arg1)) == 0) //snprintf returns num of characters
+		{
+			return NULL;
+		}
+		if ((buffer = malloc((buffSize+1) * sizeof(char))) == NULL)
+		{
+			return NULL;
+		}
+		printf("prepare m started\n");
+		sprintf_s(buffer, buffSize+1, "CLIENT_REQUEST:%s\n", arg1);
+		break;
+	
+	case CLIENT_VERSUS:
+		if ((buffSize = snprintf(NULL, 0, "CLIENT_VERSUS\n")) == 0) //snprintf returns num of characters
+		{
+			return NULL;
+		}
+
+		if ((buffer = malloc(buffSize * sizeof(char)) == NULL))
+		{
+			return NULL;
+		}
+		sprintf_s(buffer, "CLIENT_VERSUS:\n", arg1);
+		break;
+	case CLIENT_PLAYER_MOVE:
+		if ((buffSize = snprintf(NULL, 0, "CLIENT_PLAYER_MOVE:%s\n", arg1)) == 0) //snprintf returns num of characters
+		{
+			return NULL;
+		}
+
+		if ((buffer = malloc(buffSize * sizeof(char)) == NULL))
+		{
+			return NULL;
+		}
+		sprintf_s(buffer, "CLIENT_PLAYER_MOVE:%s\n", arg1);
+		break;
+	case CLIENT_DISCONNECT:
+		if ((buffSize = snprintf(NULL, 0, "CLIENT_DISCONNECT:\n")) == 0) //snprintf returns num of characters
+		{
+			return NULL;
+		}
+
+		if ((buffer = malloc(buffSize * sizeof(char)) == NULL))
+		{
+			return NULL;
+		}
+		sprintf_s(buffer, "CLIENT_DISCONNECT:\n",arg1);
+		break;
+	default:
+		buffer = NULL;
+	}
+	return buffer;
+}
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n%d\n%s\n", argv[1], atoi(argv[2]), argv[3]);
+	// Initialize Winsock
+	WSADATA wsa_data;
+	int result;
+	result = WSAStartup(MAKEWORD(2, 2), &wsa_data);
+	if (result != 0) {
+		printf("WSAStartup failed: %d\n", result);
+		return 1;
+	}
+
+	// Create and init socket
+	SOCKET client_s;
+	if ((client_s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET)
+	{
+		printf("Could not create socket : %d", WSAGetLastError());
+		return 1;
+	}
+
+	// Connect to a server.
+
+	//Create a sockaddr_in object client_addr and set values.
+	struct sockaddr_in server_addr;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(atoi(argv[2]));			// port address
+	server_addr.sin_addr.s_addr = inet_addr(argv[1]);		// IP address
+	int client_addr_len = sizeof(server_addr);
+
+	// Call the connect function, passing the created socket and the sockaddr_in structure as parameters. 
+	// Check for general errors.
+	if (connect(client_s, (SOCKADDR*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+		printf("Failed to connect due to %d.\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+
+
+	// Setup timeval variable and fd_set variable
+	struct timeval timeout;
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 0.5;
+	
+	struct fd_set fds;
+	
+	char *buffer, input[10] = { 0 };
+	buffer = PrepareMessage(CLIENT_REQUEST, "or", NULL, NULL);
+	if (buffer == NULL)
+	{
+		printf("oops");
+		return 1;
+	}
+	Sleep(10000);
+	if (sendto(client_s, buffer, strlen(buffer) - 1, 0, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
+	{
+		printf("sendto() failed with error code : %d", WSAGetLastError());
+		return 1;
+	}
+		 
+	//CLIENT_REQUEST
+//	buffer =PrepareMessage(CLIENT_REQUEST, argv[3], NULL, NULL);
+//	if (sendto(client_s, buffer, strlen(buffer)-1, 0, (struct sockaddr *) &server_addr, sizeof(server_addr)) == SOCKET_ERROR)
+//	{
+//		printf("sendto() failed with error code : %d", WSAGetLastError());
+//		return 1;
+//	}
+//	// LOOP:
+//	while(1)
+//	{
+//		//recv SERVER_MAIN_MENU
+//		while (strcmp(input, "2") && strcmp(input, "1")) 
+//		{
+//			printf("Error: Illegal command\n");
+//			printf("Choose what to do next:\n1. Play against another client\n2. Quit\n");
+//			gets(input);
+//		}
+//
+//		//VERSUS_CLIENT / DISCONNECT_CLIENT
+//		if (!strcmp(input, "1"))
+//		{
+//			//CLIENT_VERSUS
+//			buffer = PrepareMessage(CLIENT_VERSUS, NULL, NULL, NULL);
+//			if (sendto(client_s, buffer, strlen(buffer), 0, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
+//			{
+//				printf("sendto() failed with error code : %d", WSAGetLastError());
+//				return 1;
+//			}
+//		}
+//		if (!strcmp(input, "2"))
+//		{
+//			//DISCONNECT_CLIENT
+//		}
+//
+//		while (1)
+//		{
+//			//recv GAME_STARTED
+//			//recv TURN_SWITCH
+//			//recv SERVER_MOVE_REQUEST / TURN_SWITCH
+//			gets(input);
+//			buffer = PrepareMessage(CLIENT_PLAYER_MOVE, input, NULL, NULL);
+//			if (sendto(client_s, buffer, strlen(buffer), 0, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
+//			{
+//				printf("sendto() failed with error code : %d", WSAGetLastError());
+//				return 1;
+//			}
+//		}
+//	}
+	
+	
+
+	//VERSUS_CLIENT / DISCONNECT_CLIENT
+	//recv STARTED_GAME
+	// loop:
+	//recv SWITCH_TURN
+	//**wait until** recv REQUEST_MOVE_SERVER
+	//CLIENT_PLAYER_MOVE
+	//recv GAME_VIEW
+	//end loop
+	// recv ENDED_GAME
+	//end LOOP
+
+
+
+	// Deinitialize Winsock
+	result = WSACleanup();
+	if (result != 0) {
+		printf("WSACleanup failed: %d\n", result);
+		return 1;
+	}
+	free(buffer);
+	return 0;
+}
---
+++ enc_temp_folder/c1c6a0487e230e273b9f1115c272f2/main.c
@@ -0,0 +1,119 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+	// Initialize Winsock
+	WSADATA wsa_data;
+	int result;
+	result = WSAStartup(MAKEWORD(2, 2), &wsa_data);
+	if (result != 0) {
+		printf("WSAStartup failed: %d\n", result);
+		return 1;
+	}
+
+	// Create and init socket
+	SOCKET server_s;
+	if ((server_s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET)
+	{
+		printf("Could not create socket : %d", WSAGetLastError());
+		return 1;
+	}
+
+	struct sockaddr_in server_addr;
+	struct sockaddr_in client_addr;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(atoi(argv[1]));
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	int client_addrss_len, server_addrss_len = sizeof(server_addr);
+
+	//Bind
+	if (bind(server_s, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
+	{
+		printf("Bind failed with error code : %d", WSAGetLastError());
+		exit(EXIT_FAILURE);
+	}
+
+	// Setup timeval variable
+	struct timeval timeout;
+	struct fd_set fds;
+
+	timeout.tv_sec = 13;
+	timeout.tv_usec = 0;
+
+	int retval, received=0;
+	char buffer[3] = { 0 };
+
+
+	//wait for data to be sent by the channel
+
+	int ListenRes = listen(server_s, SOMAXCONN);
+	if (ListenRes == SOCKET_ERROR)
+	{
+		printf("Failed listening on socket, error %ld.\n", WSAGetLastError());
+		//goto server_cleanup_2;
+	}
+
+	SOCKET client_s = accept(server_s, NULL, NULL);
+	if (client_s == INVALID_SOCKET)
+	{
+		printf("Accepting connection with client failed, error %ld\n", WSAGetLastError());
+		//goto server_cleanup_3;
+	}
+
+	while (1)
+	{ // &client_addr
+		
+	  //set descriptors
+		FD_ZERO(&fds);
+		FD_SET(client_s, &fds);
+
+		retval = select(max(server_s, stdin) + 1, &fds, NULL, NULL, &timeout);
+		if (retval < 0)
+		{//error occured
+			return 1;
+			// check errno/WSAGetLastError(), call perror(), etc ...
+		}
+
+		if (retval > 0)
+		{
+			if (FD_ISSET(client_s, &fds))
+			{//data was received from the channel
+			//we will process it (decode) and write it to the output file
+
+				//Receive data from the channel
+				int buffer_len = recvfrom(client_s, buffer, 2, 0, (struct sockaddr*)&server_addr, &server_addrss_len); // ********* replace with client address
+				if (buffer_len == 0)
+				{
+					return 1;
+				}
+				//printf(buffer);
+				received += buffer_len;
+				break;
+			}
+		}
+		if (retval == 0)
+		{
+			continue;
+		}
+	}
+	free(buffer);
+	// Deinitialize Winsock
+	result = WSACleanup();
+	if (result != 0) {
+		printf("WSACleanup failed: %d\n", result);
+		return 1;
+	}
+	return 0;
+}
